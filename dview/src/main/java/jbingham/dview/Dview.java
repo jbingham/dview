8e5eaefccb7999083e3a1fa550f714efea6f24e1
Jonathan Bingham <binghamj@google.com>
2017-06-04 18:21:37 -0700

    add dependencies to pom

---
+++ dview/src/main/java/jbingham/dview/Dview.java
@@ -0,0 +1,13 @@
+package jbingham.dview;
+
+/**
+ * Hello world!
+ *
+ */
+public class Dview 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
